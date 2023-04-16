31300a6ca33743c38acaaa357b669222a8d6ef28
sombrerosheep <sombrerosheep@gmail.com>
2023-04-16 10:51:36 -0500

    basic player movement

--- src/game/playership/mod.rs
+++ src/game/playership/mod.rs
@@ -4,13 +4,23 @@ mod systems;
 use bevy::prelude::*;
 
 use crate::game::GameState;
+use crate::AppState;
 use systems::*;
 
 pub struct PlayerShipPlugin;
 
 impl Plugin for PlayerShipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_player.in_schedule(OnEnter(GameState::Playing)))
-            .add_system(despawn_player.in_schedule(OnExit(GameState::Playing)));
+        app
+            // OnEnter
+            .add_system(spawn_player.in_schedule(OnEnter(GameState::Playing)))
+            // OnExit
+            .add_system(despawn_player.in_schedule(OnExit(GameState::Playing)))
+            // Systems
+            .add_system(
+                player_movement
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(GameState::Playing)),
+            );
     }
 }
--- src/game/playership/systems.rs
+++ src/game/playership/systems.rs
@@ -2,6 +2,8 @@ use bevy::prelude::*;
 
 use crate::game::playership::components::*;
 
+const PLAYERSHIP_SPEED: f32 = 125.0;
+
 pub fn spawn_player(mut commands: Commands) {
     print!("spawning player");
     commands.spawn((
@@ -21,5 +23,35 @@ pub fn spawn_player(mut commands: Commands) {
 pub fn despawn_player(mut commands: Commands, playership_query: Query<Entity, With<PlayerShip>>) {
     if let Ok(player_ship) = playership_query.get_single() {
         commands.entity(player_ship).despawn();
+    }
+}
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>, // todo: input resource to abstract all input kinds
+    mut player_query: Query<&mut Transform, With<PlayerShip>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction.x = -1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction.x = 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction.y = 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction.y = -1.0;
+        }
+
+        direction = direction.normalize_or_zero();
+
+        transform.translation += direction * PLAYERSHIP_SPEED * time.delta_seconds();
     }
 }
